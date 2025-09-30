1ec21836b505434575f209b1ba0890450e575aa8
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-09-30 14:19:25 +0200

    something

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,8 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function CitySearch() {
 	const API_KEY = import.meta.env.VITE_API_KEY;
 	const API_URL = import.meta.env.VITE_GEO_API_URL;
+	const [city, setCity] = useState(null);
+
+	const url = new URL(API_URL);
+
+	url.get(lat, lon, (appid = { API_KEY }));
 
 	useEffect(() => {
 		const cityData = async (city) => {
